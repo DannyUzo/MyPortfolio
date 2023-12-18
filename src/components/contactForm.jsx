901d7356af0f5e03cf7dc5689d4bd6cfa48b3f05
Danny Uzo <uzodinmadaniel42@gmail.com>
2023-12-18 19:17:38 +0100

    browser updates

--- src/components/contactForm.jsx
+++ src/components/contactForm.jsx
@@ -10,36 +10,42 @@ export const ContactForm = () => {
     <form onSubmit={handleSubmit}>
       <h2>Get in touch</h2>
       <div className="formWrapper">
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          type="name"
-          name="name"
-          placeholder="What's your name?"
-          required
-        />
-        <ValidationError prefix="Name" field="name" errors={state.errors} />
-        <label htmlFor="email">Email Address</label>
-        <input
-          id="email"
-          type="email"
-          name="email"
-          placeholder="What's your web address?"
-          required
-        />
-        <ValidationError prefix="Email" field="email" errors={state.errors} />
-        <label htmlFor="message">Message</label>
-        <textarea
-          placeholder="What do you want to say?"
-          id="message"
-          name="message"
-          required
-        />
-        <ValidationError
-          prefix="Message"
-          field="message"
-          errors={state.errors}
-        />
+        <div className="infoWrapper">
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            type="name"
+            name="name"
+            placeholder="What's your name?"
+            required
+          />
+          <ValidationError prefix="Name" field="name" errors={state.errors} />
+        </div>
+        <div className="infoWrapper">
+          <label htmlFor="email">Email Address</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            placeholder="What's your web address?"
+            required
+          />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+        </div>
+        <div className="infoWrapper">
+          <label htmlFor="message">Message</label>
+          <textarea
+            placeholder="What do you want to say?"
+            id="message"
+            name="message"
+            required
+          />
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+        </div>
         <button type="submit" disabled={state.submitting}>
           Submit
         </button>
