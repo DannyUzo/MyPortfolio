c637f817c05b6579f74c9b96e87f2d27ec274fc5
Danny Uzo <uzodinmadaniel42@gmail.com>
2024-06-15 21:50:55 +0100

    updates: experience section added

--- src/Pages/Qualifications.jsx
+++ src/Pages/Qualifications.jsx
@@ -3,34 +3,27 @@ import React from "react";
 import { Fade } from "react-awesome-reveal";
 import { Skills } from "../constants";
 import SkillCard from "../components/SkillCard";
+import Experience from "../components/Experience";
 
 const Qualifications = () => {
   return (
     <div className="qualifications">
       <div className="main">
-        <Fade triggerOnce>
-          <h2>
-            Here are some of my Qualifications<span> .</span>{" "}
-          </h2>
-        </Fade>
-        <Fade>
-          <p className="skillText">
-            I'm a frontend web developer proficient in HTML5, CSS3, JavaScript,
-            and React, crafting visually stunning and responsive designs. I
-            excel in optimizing web performance, and ensuring web
-            accessibility. With expertise in Tailwind CSS, SCSS, and Next.js, I
-            manage projects efficiently using Git version control and project
-            management tools, guaranteeing cross-browser compatibility for
-            seamless user experiences.
-          </p>
-        </Fade>
         <div className="skills">
+        <Fade triggerOnce>
+          <div>
+          <strong>Experience</strong>
+          <Experience />
+
+          </div>
+            <div>
           <strong>Skills</strong>
-          <Fade>
             <div className="skillImages">
               {Skills.map((Language) => (
                 <SkillCard key={Language.id} {...Language} />
               ))}
+            </div>
+
             </div>
           </Fade>
         </div>
