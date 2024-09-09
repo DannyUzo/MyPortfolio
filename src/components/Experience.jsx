27d076f78030969595ef88d214b4cfd40b228f02
Danny Uzo <uzodinmadaniel42@gmail.com>
2024-09-09 19:41:01 +0100

    Updates: Updated experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,3 @@
-
-
 const Experience = () => {
   return (
     <div className="experience">
@@ -7,18 +5,18 @@ const Experience = () => {
         <h3>BuildBridge, Abeokuta — Frontend Developer</h3>
         <p className="date">OCT 2023 - PRESENT</p>
         <ul>
-          <li>Developing user-friendly web interfaces and implementing designs provided by UI/UX teams.</li>
-          <li>Collaborating with backend developers to integrate frontend components with server-side logic.</li>
-          <li>Ensuring cross-browser compatibility and responsiveness of web applications.</li>
-        </ul>
-      </div>
-      <div className="job">
-        <h3>GCCC, Lagos — Frontend Developer</h3>
-        <p className="date">SEP 2023 - NOV 2023 (contract)</p>
-        <ul>
-          <li>Contributed to the development of frontend components for client projects.</li>
-          <li>Implemented responsive designs and optimized web applications for performance.</li>
-          <li>Worked closely with project managers to meet deadlines and deliver high-quality products.</li>
+          <li>
+            Developing user-friendly web interfaces and implementing designs
+            provided by UI/UX teams.
+          </li>
+          <li>
+            Collaborating with backend developers to integrate frontend
+            components with server-side logic.
+          </li>
+          <li>
+            Ensuring cross-browser compatibility and responsiveness of web
+            applications.
+          </li>
         </ul>
       </div>
       <div className="job">
@@ -26,8 +24,25 @@ const Experience = () => {
         <p className="date">DEC 2023 - PRESENT</p>
         <ul>
           <li>Assisting senior developers in frontend development tasks.</li>
-          <li>Gaining hands-on experience with JavaScript, HTML, CSS, and various frontend frameworks.</li>
-          <li>Participating in team meetings and contributing ideas to improve project workflows.</li>
+          <li>
+            Gaining hands-on experience with JavaScript, HTML, CSS, and various
+            frontend frameworks.
+          </li>
+          <li>
+            Participating in team meetings and contributing ideas to improve
+            project workflows.
+          </li>
+        </ul>
+      </div>
+      <div className="job">
+        <h3>Zuri Internship(HNGx) — Frontend Intern Trainee</h3>
+        <p className="date">SEP 2023 - OCT 2023</p>
+        <ul>
+          <li>Completed several internship projects.</li>
+          <li>
+            Worked closely with project managers and backend engineers to meet
+            deadlines and deliver high-quality products.
+          </li>
         </ul>
       </div>
     </div>
